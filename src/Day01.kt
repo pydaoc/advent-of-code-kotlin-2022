1d9f508581c72f43c11c47bc2880b0db5f6f0e89
Pierre Yves DARRAS <pierre-yves.darras@epsilon-france.com>
2022-12-05 18:39:02 +0100

    solution for Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,45 @@
+import kotlin.math.*
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var referenceCalories = 0
+        var currentCalories = 0
+
+        for (line in input) {
+            if (line.isEmpty()) {
+                referenceCalories = max(referenceCalories, currentCalories)
+                currentCalories = 0
+            } else {
+                val calories = line.toInt()
+                currentCalories += calories
+            }
+        }
+
+        referenceCalories = max(referenceCalories, currentCalories)
+
+        return referenceCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        var currentCalories = 0
+        val elfCalories = arrayListOf<Int>()
+
+        for (line in input) {
+            if (line.isEmpty()) {
+                elfCalories.add(currentCalories)
+                currentCalories = 0
+            } else {
+                val calories = line.toInt()
+                currentCalories += calories
+            }
+        }
+
+        elfCalories.add(currentCalories)
+        val sortedElfCalories = elfCalories.sortedDescending()
+
+        return sortedElfCalories[0] + sortedElfCalories[1] + sortedElfCalories[2]
+    }
 
     val input = readInput("Day01")
     println(part1(input))
